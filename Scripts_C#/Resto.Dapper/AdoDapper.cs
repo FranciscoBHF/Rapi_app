6fd5301649dfea88e3284d5807488583051ae4ba
Fran <fran.basualdo.heffner@gmail.com>
2023-11-08 16:18:56 -0300

    la primera parte del Dapper

--- Scripts_C#/Resto.Dapper/AdoDapper.cs
+++ Scripts_C#/Resto.Dapper/AdoDapper.cs
@@ -1,10 +1,24 @@
 ﻿using Biblioteca;
+using MySqlConnector;
+using System.Data;
+using Dapper;
 
 namespace Resto.Dapper;
 public class AdoDapper : IAdo
 {
+    private readonly IDbConnection _conexion;
+    public AdoDapper(IDbConnection conexion) => _conexion = conexion;
+    public AdoDapper(string cadena)
+        => _conexion = new MySqlConnection(cadena);
+    
+    #region 
+    private static readonly string _queryClientePass
+        = @"select *
+        from Cliente
+        where email = @unemail
+        and pass = SHA2(@unpass, 256)
+        LIMIT 1";
     public Cliente? ClientePorPass(string email, string pass)
-    {
-        throw new NotImplementedException();
-    }
+        => _conexion.QueryFirstOrDefault<Cliente>(_queryClientePass, new {unEmail = email, unPass = pass});
+    #endregion
 }
