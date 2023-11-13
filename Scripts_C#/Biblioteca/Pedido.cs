28262696ab7622412d71beb2fa8757403454bc90
sara <sara1330castro@gmail.com>
2023-11-13 14:26:39 -0300

    roberto

--- Scripts_C#/Biblioteca/Pedido.cs
+++ Scripts_C#/Biblioteca/Pedido.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Biblioteca;
 
-namespace Biblioteca
+public class Pedido
 {
-    public class Pedido
+    public int id {get ; set ;}
+    public DateTime fecha {get ; set ;}
+    public float valoracion {get ; set ;}
+    public string descripcion {get ; set ;}
+        public Pedido(int id, DateTime fecha, float valoracion, string descripcion)
     {
-        public int id {get ; set ;}
-        public DateTime fecha {get ; set ;}
-        public float valoracion {get ; set ;}
-        public string descripcion {get ; set ;}
-            public Pedido(int id, DateTime fecha, float valoracion, string descripcion)
-        {
-            this.id = id;
-            this.fecha = fecha;
-            this.valoracion = valoracion;
-            this.descripcion = descripcion;
-        } 
-    }
+        this.id = id;
+        this.fecha = fecha;
+        this.valoracion = valoracion;
+        this.descripcion = descripcion;
+    } 
 }
