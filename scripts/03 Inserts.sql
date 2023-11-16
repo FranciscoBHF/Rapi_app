508878b4e5362e7c6f488aa5b081adc900f62826
dapugliese <dapugliese@gmail.com>
2023-11-16 14:48:40 -0300

    Arreglos en el codigo y agregacion de datos

--- scripts/03 Inserts.sql
+++ scripts/03 Inserts.sql
@@ -5,10 +5,15 @@ DELIMITER $$
 CALL RegistrarCliente1(1, 'roberto@gmail.com', 'roberto', 'guaymayen', '321')$$
 Call  RegistrarCliente1 (2,  "Robert@gmail.com","Rupert", "orci", "Pelele")$$
 CALL RegistrarCliente1(3, 'Chema@gmail.com', 'Chems', 'Chep', 'Palete')$$
-CALL AltaRestaurante(1, 'Las palmitas','pepelepu500','pe','LasPalmitas@gmail.com')$$
-CALL AltaRestaurante(2, 'La rioja','Obo2000','Che','LaRioja@gmail.com')$$
-CALL AltaRestaurante(2, 'El tango','Obo2000','Che','LaRioja@gmail.com')$$
+CALL AltaRestaurante(1, 'Las Palmitas','pepelepu500','pe','LasPalmitas@gmail.com')$$
+CALL AltaRestaurante(2, 'La Rioja','Obo2000','LaRioja@gmail.com','Che')$$
+CALL AltaRestaurante(3, 'El Tango','Apollo773','Nashe@gmail.com','Odisea2001')$$
 CALL AltaPlato(1,1,'pizz-a','Una pizza clasita con queso y salsa', 500, TRUE)$$
+CALL AltaPlato(2,1,'empa-nada','Una empanada vacia', 900, TRUE)$$
+CALL AltaPlato(3,2,'El Rio','Sopa de carne con espinaca', 700, TRUE)$$
+CALL AltaPlato(4,2,'Bife','Es un bife pero de la Rioja', 800, TRUE)$$
+CALL AltaPlato(5,3,'La Nave','Un Pastel de carne hecho con forma de nave', 1500, TRUE)$$
+CALL AltaPlato(6,3,'HAL','Una milanesa con salsa en el medio con forma circular', 900, TRUE)$$
 CALL AltaPedido( 1, '2000-02-29' , 2.5 , 'res papas con chedar y una pizz-a',1,1)$$
 CALL AltaPedido( 2, '2000-03-10' , 2.5 , 'res papas con chedar y una pizz-a',1,1)$$
 CALL AltaPlatoPedido(1,1,4,1000.50)$$
