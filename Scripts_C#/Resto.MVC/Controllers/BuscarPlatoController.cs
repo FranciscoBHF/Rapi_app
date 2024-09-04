b903bd9b212a87d8704d1df4f8f206daad698238
AxelMtz1 <114775884+AxelMtz1@users.noreply.github.com>
2024-09-04 15:26:30 -0300

    Avances

--- Scripts_C#/Resto.MVC/Controllers/BuscarPlatoController.cs
+++ Scripts_C#/Resto.MVC/Controllers/BuscarPlatoController.cs
@@ -1,17 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Biblioteca;
-using System.Threading.Tasks;
 
 namespace SuperSimple.Mvc.Controllers
 {
     public class BuscarPlatoController : Controller
     {
         protected readonly IAdo Ado;
-        private static readonly string _cadena =
-            @"Server=localhost;Database=5to_comidapp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
-        public BuscarPlatoController()
+        public BuscarPlatoController(IAdo ado)
         {
-            Ado = new Resto.Dapper.AdoDapper(_cadena);
+            Ado = ado;
         }
         [HttpGet]
         //public async Task<IActionResult> Detalle(int id)
@@ -25,26 +22,28 @@ namespace SuperSimple.Mvc.Controllers
         //    }
         //    return View(plato);
         //}
-        //public async Task<IActionResult> BuscarPlato(string? cadena)
-        //{
-        //                var platos = await Ado.BuscarPlato(cadena);
-        //    if (platos == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(plato);
-        //}
-
-        public async Task<IActionResult> Detalle(string? cadena)
+        public async Task<IActionResult> BuscarPlato(string? cadena)
         {
-            if (cadena == null)
-            return NotFound();
+            if(cadena is null)
+                return NotFound();
             var platos = await Ado.buscarPlato(cadena);
             if (platos == null)
             {
                 return NotFound();
             }
             return View(platos);
+        }
+
+        public async Task<IActionResult> Detalle(string? cadena)
+        {
+            if (cadena == null)
+                return NotFound();
+            var platos = await Ado.buscarPlato(cadena);
+            if (platos == null)
+            {
+                return NotFound();
+            }
+            return View("ListaPlatos", platos);
         }
     }
 }
--- Scripts_C#/Resto.MVC/Controllers/PlatoController.cs
+++ Scripts_C#/Resto.MVC/Controllers/PlatoController.cs
@@ -1,38 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Biblioteca;
-using System.Threading.Tasks;
 
-namespace SuperSimple.Mvc.Controllers
+namespace SuperSimple.Mvc.Controllers;
+
+public class PlatoController : Controller
 {
-    public class PlatoController : Controller
-    {
     protected readonly IAdo Ado;
-    private static readonly string _cadena =
-        @"Server=localhost;Database=5to_comidapp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
-    public PlatoController() 
+    public PlatoController(IAdo ado) => Ado = ado;
+
+    [HttpGet("plato")]
+    public async Task<IActionResult> Index()
     {
-        Ado = new Resto.Dapper.AdoDapper(_cadena);
+        var platos = await Ado.TodosPlatos();
+        return View("ListaPlatos", platos);
     }
 
-        [HttpGet("plato")]
-        public async Task<IActionResult> Index()
+    [HttpGet]
+    public async Task<IActionResult> Detalle(int id)
+    {
+        var platos = await Ado.TodosPlatos();
+        var plato = platos.FirstOrDefault(p => p.id == id);
+
+        if (plato == null)
         {
-            var platos = await Ado.TodosPlatos();
-            return View("ListaPlatos", platos);
+            return NotFound();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Detalle(int id)
-        {
-            var platos = await Ado.TodosPlatos();
-            var plato = platos.FirstOrDefault(p => p.id == id);
-
-            if (plato == null)
-            {
-                return NotFound();
-            }
-
-            return View(plato);
-        }
+        return View(plato);
     }
 }
