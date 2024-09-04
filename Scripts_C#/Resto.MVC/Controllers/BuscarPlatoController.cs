bca22bdd14b1d14e2c0fded36c90d5cfd364377f
FranciscoBHF <114776184+FranciscoBHF@users.noreply.github.com>
2024-09-04 14:23:14 -0300

    Avances
    Hice el controller pero sin view no puedo probarlo

--- Scripts_C#/Resto.MVC/Controllers/BuscarPlatoController.cs
+++ Scripts_C#/Resto.MVC/Controllers/BuscarPlatoController.cs
@@ -6,35 +6,45 @@ namespace SuperSimple.Mvc.Controllers
 {
     public class BuscarPlatoController : Controller
     {
-    protected readonly IAdo Ado;
-    private static readonly string _cadena =
-        @"Server=localhost;Database=5to_comidapp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
-    public BuscarPlatoController() 
-    {
-        Ado = new Resto.Dapper.AdoDapper(_cadena);
-    }
-        public async Task<IActionResult> Index()
+        protected readonly IAdo Ado;
+        private static readonly string _cadena =
+            @"Server=localhost;Database=5to_comidapp;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
+        public BuscarPlatoController()
         {
-
-            var platos = await Ado.buscarPlato(nombre);
-            return View("ListaPlatos", platos);
+            Ado = new Resto.Dapper.AdoDapper(_cadena);
         }
-
         [HttpGet]
-        public async Task<IActionResult> Detalle(int id)
-        {
-            var platos = await Ado.TodosPlatos();
-            var plato = platos.FirstOrDefault(p => p.id == id);
+        //public async Task<IActionResult> Detalle(int id)
+        //{
+        //    var platos = await Ado.TodosPlatos();
+        //    var plato = platos.FirstOrDefault(p => p.id == id);
 
-            if (plato == null)
+        //    if (plato == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(plato);
+        //}
+        //public async Task<IActionResult> BuscarPlato(string? cadena)
+        //{
+        //                var platos = await Ado.BuscarPlato(cadena);
+        //    if (platos == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(plato);
+        //}
+
+        public async Task<IActionResult> Detalle(string? cadena)
+        {
+            if (cadena == null)
+            return NotFound();
+            var platos = await Ado.buscarPlato(cadena);
+            if (platos == null)
             {
                 return NotFound();
             }
-            return View(plato);
-        }
-        public async Task<IActionResult> BuscarPlato(string? cadena)
-        {
-            
+            return View(platos);
         }
     }
 }
