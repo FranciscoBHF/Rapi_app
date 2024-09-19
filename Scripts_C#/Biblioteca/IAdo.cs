4bd32f1c7a485c5e543f0cb653db5e4b766adb5f
FranciscoBHF <114776184+FranciscoBHF@users.noreply.github.com>
2024-09-19 15:14:20 -0300

    Update iado
    Lo hice un poco mas entendible

--- Scripts_C#/Biblioteca/IAdo.cs
+++ Scripts_C#/Biblioteca/IAdo.cs
@@ -12,16 +12,17 @@ public interface IAdo
 
     //Restaurante
     void Restaurante(Restaurant restaurant);
+    Task <List<Restaurant>> TodosRestaurants();
     Restaurant? RestaurantPorPass(string email, string pass);
     Task<Restaurant?> RestaurantPorPassAsync(string email, string pass);
     void AltaRestaurant(Restaurant nuevoPapa, string pasword);
     Task AltaRestaurantAsync(Restaurant restaurant, string pasword);
-    // Metodo Asincronico
-    Task AltaRestauranteAsync(Restaurant restaurant);
     Task <List<Restaurant>> buscarRestaurant (string restaurante);
+
+    //platos
     Task <List<Plato>> buscarPlato(string nombre);
     Task <List<Plato>> TodosPlatosAsync();
     void AltaPlato(Plato plato, Plato idRestaurant);
     Task AltaPlatoAsync(Plato plato, Plato idRestaurant);
-    Task <List<Restaurant>> TodosRestaurants();
+    
 }
