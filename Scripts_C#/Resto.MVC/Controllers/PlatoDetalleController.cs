9c0c3feb2e28af9394754b3da185e5d2a283ea1e
Axel <axeleonelmtz8@gmail.com>
2024-10-03 15:49:00 -0300

    update

--- Scripts_C#/Resto.MVC/Controllers/PlatoDetalleController.cs
+++ Scripts_C#/Resto.MVC/Controllers/PlatoDetalleController.cs
@@ -1,19 +1,19 @@
-// using Microsoft.AspNetCore.Mvc;
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
-// using Biblioteca;
-// using Resto.MVC.Controllers.Modal;
-
-// namespace Resto.MVC.Controllers
-// {
-//     public class PlatoDetalleController : Controller
-//     {
-//         protected readonly IAdo _Ado;
-//         public PlatoDetalleController(IAdo ado) => _Ado = ado;
-//         [HttpGet]
-//         public async Task<IActionResult> DetallePlato()
-//         {
-            
-//         }
-//     }
-// }
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Biblioteca;
+using Resto.MVC.Controllers.Modal;namespace Resto.MVC.Controllers
+{
+    public class PlatoDetalleController : Controller
+    {
+        protected readonly IAdo _Ado;
+        public PlatoDetalleController(IAdo ado) => _Ado = ado;
+        
+        [HttpGet]
+        public async Task<IActionResult> ObtenerDetalle()
+        {
+            var platos = await _Ado.DetallePlatoAsync();
+            return View ("DetallePlato", platos);
+        }
+    }
+}
